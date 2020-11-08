c00e0f8ede1efa68f1b2706620cc3b929282248c
vedantpople <vedantpople4@gmail.com>
2020-11-08 22:49:33 +0530

    new code

--- Reverse the array.cpp
+++ Reverse the array.cpp
@@ -7,7 +7,17 @@ using namespace std;
 
 int main()
 {
-
+    int n,temp=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n/2;i++){
+        temp = a[i];
+		a[i] = a[i-1];
+        
+        a[i-1]=temp;
+    }
+    for(int i=0;i<n;i++)cout<<a[i];
 
     return 0;
 }
