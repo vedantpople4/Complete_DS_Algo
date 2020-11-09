47892238bf6670d5cb6bb4ccb3e1bc28ab056d4c
vedantpople <vedantpople4@gmail.com>
2020-11-09 22:20:07 +0530

    reverse the array update

--- Reverse the array.cpp
+++ Reverse the array.cpp
@@ -5,19 +5,33 @@ using namespace std;
 #define ll long long 
 #define testcase long long t; cin>>t;
 
+void reverse(int arr[], int start, int end)
+{
+	if(start>=end)
+	return;
+	int temp = arr[start];
+	arr[start] = arr[end];
+	arr[end] = temp;
+	
+	reverse(arr, start+1, end-1);
+}
+
+void printarr(int arr[], int k)
+{
+	for(int i=0;i<k;i++)
+	cout<<arr[i]<<" "<<endl;
+}
+
+
+
 int main()
 {
     int n,temp=0;
     cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++) cin>>a[i];
-    for(int i=0;i<n/2;i++){
-        temp = a[i];
-		a[i] = a[i-1];
-        
-        a[i-1]=temp;
-    }
-    for(int i=0;i<n;i++)cout<<a[i];
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    reverse(arr,0,n-1);
+    printarr(arr,n);
 
     return 0;
 }
