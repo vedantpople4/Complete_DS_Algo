5a89c172dae0bd8eddc4c4797a0ddde40638a16d
Vedant pople <vedantpople4@gmail.com>
2020-11-14 22:58:33 +0530

    Update union and intersection  in arrays.cpp

--- union and intersection  in arrays.cpp
+++ union and intersection  in arrays.cpp
@@ -1,28 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define nl '\n'
+#define ll long long
+#define testcase long long t; cin>>t;
+
+
+void printUnion(int a[], int b[], int n, int m){
+	set<int> hs;
+	for(int i=0;i<n;i++) hs.insert(a[i]);
+	for(int i=0;i<m;i++) hs.insert(b[i]);
+	for(auto it = hs.begin(); it!=hs.end(); it++) cout<< * it <<" ";
+	cout<<endl;
+}
+
+void printIntersection(int a[], int b[], int n, int m){
+	set<int> hs;
+	for(int i=0;i<n;i++) hs.insert(a[i]);
+	for(int i=0;i<m;i++)
+	if(hs.find(b[i]) != hs.end())
+	cout<<b[i]<<" ";
+}
 int main()
 {
-	long long t;
-	cin>>t;
-	while(t--){
-		int n,m;
-		cin>>n>>m;
-		int a[n],b[m];
-		vector <int> v;
-		for(int i=0;i<n;i++) {
-		cin>>a[i];
-		v.push_back(a[i]);
-		}
-		for(int i=0;i<m;i++) cin>>b[i];
-		for(int i=0,it=v.begin();i<m,it!=v.end();i++,it++){
-			if(b[i]!=v[it])
-			v.push_back(b[i]);
-		}
-		for(int i=v.begin();i!=v.end();i++) cout<<v[i]<<endl;
-		
-
-	}
+testcase;
+while(t--){
+	int n,m;
+	cin>>n>>m;
+	int a[n],b[m];
+	for(int i=0;i<n;i++) cin>>a[i];
+	for(int i=0;i<m;i++) cin>>b[i];
+	printUnion(a , b, n, m);
+	printIntersection(a, b, n ,m);
+}
 	
 	
 	return 0;
